62390cf646bf99c60f24b452382d5ec05f07c48c
stanislavHamara <stanislav.hamara@gmail.com>
2016-09-01 10:11:53 +0100

    Add [CurrencyPaid] to [HumanResources.Employee]

---
+++ AdventureWorks.Database/Migrations/0008_HumanResources.Employee_AddCurrencyPaid.sql
@@ -0,0 +1,8 @@
+﻿-- <Migration ID="fb95093e-c390-4580-a4c1-a4d2d1a6fc22" />
+GO
+
+PRINT N'Altering [HumanResources].[Employee]'
+GO
+ALTER TABLE [HumanResources].[Employee] ADD
+[CurrencyPaid] [nchar] (10) NULL
+GO
